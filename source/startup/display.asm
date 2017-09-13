29d9f9da44fa558f335cd7cd6caca7326fe16afa
ljvblfz <ljvblfz@users.noreply.github.com>
2017-09-13 12:11:08 +0800

    Create display.asm

---
+++ source/startup/display.asm
@@ -0,0 +1,24 @@
+;++
+;
+; Module namme
+;
+;       display.asm
+;
+; Abstract
+;
+;       video display
+;
+;--
+
+
+;
+; clear screen
+;
+ClearScreen:
+    ret
+
+;
+; show string
+;
+puts:
+    ret
